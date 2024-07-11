1ae2e5c4ee221c4937dbfa63cbeb0be732dfe60b
subhashask <subhashkumar9540123@gmail.com>
2024-07-11 14:32:39 +0530

    Api Issue

--- ContextApi.tsx
+++ ContextApi.tsx
@@ -14,7 +14,6 @@ import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined
 import LogoutIcon from "@mui/icons-material/Logout";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import {SideBarMenu,SingleNoteType,DarkModeType} from "./app/Types/Types";
 interface GlobalContextType {
   sideBarMenuObject: {
     sideBarMenu: SideBarMenu[];
@@ -41,6 +40,28 @@ interface GlobalContextType {
     allNotes: SingleNoteType[];
     setAllNotes: React.Dispatch<React.SetStateAction<SingleNoteType[]>>;
   };
+}
+
+interface SideBarMenu {
+  id: number;
+  name: string;
+  isSelected: boolean;
+  icon: React.ReactNode;
+}
+interface DarkModeType {
+  id: number;
+  isSelected: boolean;
+  icon: React.ReactNode;
+}
+interface SingleNoteType {
+  id: string;
+  title:string;
+  isFavorite:boolean;
+  tags:string[];
+  description:string;
+  code:string;
+  language:string;
+  creationDate:string;
 }
 
 const ContextProvider = createContext<GlobalContextType>({
